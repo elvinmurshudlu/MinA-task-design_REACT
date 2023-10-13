b16395a8251d0becdbf82f2668084892b4e5f3bd
elvinmurshudlu <murshudlu.elvin@gmail.com>
2023-10-14 01:27:12 +0400

    refactor

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -7,17 +7,17 @@ export function Card({card}){
     return (
        
         
-        <Link  to={'/cards/'+card.id} className={style['card-item'] + " d-flex"} >
-               <img className={style['img']} src={card.img} alt=""/>
-  <div className={style['content'] + '  d-flex flex-column justify-content-between align-items-end'} >
+        <Link  to={'/cards/'+card.id} className="card-item d-flex" >
+               <img className='img' src={card.img} alt=""/>
+  <div className='content d-flex flex-column justify-content-between align-items-end' >
 
-    <div className={style["vote"]}>Vote:</div>
-    <div className={style['content-footer'] + ' d-flex justify-content-between'} >
-      <div className={style["content-footer_text"]}>
-        <h6 className={style["content-footer_title"]}>{card.title}</h6>
-        <p className={style["content-footer_subtitle"]}>{card.subtitle}</p>
+    <div className="vote">Vote:</div>
+    <div className='content-footer d-flex justify-content-between' >
+      <div className="content-footer_text">
+        <h6 className="content-footer_title">{card.title}</h6>
+        <p className="content-footer_subtitle">{card.subtitle}</p>
       </div>
-      <div className={style['content-footer_icons'] + ' d-flex align-items-end justify-content-end gap-1 '} >
+      <div className='content-footer_icons d-flex align-items-end justify-content-end gap-1 ' >
          <BsFillHeartFill></BsFillHeartFill>
           <BsLink45Deg></BsLink45Deg>
           <BsFillHandIndexFill></BsFillHandIndexFill>
